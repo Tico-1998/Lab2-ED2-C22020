003299c7423c36f0c3e4981d01b4fe9db3c0f365
Carlos Andrés Vargas Orué <voca98@hotmail.com>
2020-09-26 02:09:01 -0600

    Add files via upload

--- Lab 2 ED2C22020/Nodo.cs
+++ Lab 2 ED2C22020/Nodo.cs
@@ -108,6 +108,131 @@ public class Nodo
 				//corregir underflow
             }
         }
+		public void InOrden(Nodo(T))//el valor que se recibe es la raiz
+        {
+			//ubicar raiz 
+			Nodo raiz = new Nodo(T);
+			Nodo nodohijo = new Nodo(T);
+			if(raiz.hijo[0]!=null)
+            {
+				nodohijo = raiz.hijo[0];
+				InOrden(nodohijo);
+            }
+			else
+            {
+				//Escribir valores del nodo
+				raiz = nodohijo.padre;				
+				//escribir raiz.valor[0];
+				if(raiz.hijo[1]!=null)
+                {
+					nodohijo = raiz.hijo[1];
+					InOrden(nodohijo);
+                }
+				else
+                {
+					//Escribir valores del nodo
+					raiz = nodohijo.padre;
+                    //escribir raiz.valor[1];
+                    if (raiz.hijo[2]!=null)
+                    {
+						nodohijo = raiz.hijo[2];
+						InOrden(nodohijo);
+                    }
+					else
+                    {
+						//escribir valores del nodo
+						raiz = nodohijo.padre;
+                        //Escribir raiz.valor[2];
+                        if (raiz.hijo[3]!=null)
+                        {
+							nodohijo = raiz.hijo[3];
+							InOrden(nodohijo);
+                        }
+						else
+                        {
+							//escribir valores del nodo
+							raiz = nodohijo.padre;
+                            //escribir raiz.valor[3];
+                            if (raiz.hijo[4]!=null)
+                            {
+								nodohijo = raiz.hijo[4];
+								InOrden(nodohijo);
+                            }
+                            else
+                            {
+								//escribir valor del nodo
+                            }
+                        }
+                    }
+				}
+			}
+
+        }
+		public void PreOrden(Nodo(T))
+        {
+			Nodo Raiz = new Nodo(T);
+			//imprimir valor 
+			Nodo nodohijo = new Nodo(T);
+            if (Raiz.hijo[0]!=null)
+            {
+				nodohijo = Raiz.hijo[0];
+				PreOrden(nodohijo);
+            }
+            else if (Raiz.hijo[1]!=null)
+            {
+				nodohijo = Raiz.hijo[1];
+				PreOrden(nodohijo);
+            }
+            else if (Raiz.hijo[2]!=null)
+            {
+				nodohijo = Raiz.hijo[2];
+				PreOrden(nodohijo);
+            }
+			else if (Raiz.hijo[3]!=null)
+            {
+				nodohijo = Raiz.hijo[3];
+				PreOrden(nodohijo);
+            }
+            else if (Raiz.hijo[4]!=null)
+            {
+				nodohijo = Raiz.hijo[4];
+				PreOrden(nodohijo);
+            }
+        }
+		public void PostOrden(Nodo(T))
+        {
+			Nodo raiz = new Nodo(T);
+			Nodo nodohijo = new Nodo(T);
+			if(raiz[0]!=null)
+            {
+				nodohijo = raiz[0];
+				PostOrden(nodohijo);
+            }
+			else if(raiz[1]!=null)
+            {
+				nodohijo = raiz[1];
+				PostOrden(nodohijo);
+            }
+			else if(raiz[2]!=null)
+            {
+				nodohijo = raiz[2];
+				PostOrden(nodohijo);
+            }
+			else if(raiz[3]!=null)
+            {
+				nodohijo = raiz[3];
+				PostOrden(nodohijo);
+            }
+			else if(raiz[4]!=null)
+            {
+				nodohijo = raiz[4];
+				PostOrden(nodohijo);
+            }
+			else
+            {
+				//escribir valores 
+            }
+        }
 		
 	}
 }
